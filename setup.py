9a8585c09f37c578a7e6c76763bbcf6632826bf5
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-06-21 23:18:58 +0000

    yes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='Brython Typed',
+    name='brython-typed',
     version='1.0.0',
     description="Types and IDE support for Brython 3.9.4.",
     url='https://github.com/ENDERZOMBI102/brython-typed',
@@ -12,16 +12,20 @@ setup(
     author_email='enderzombi102.end@gmail.com',
     license='mit',
 
+    project_urls={
+        "Bug Tracker": "https://github.com/ENDERZOMBI102/brython-typed/issues",
+    },
+
     keywords='brython,python,browser',
     classifiers=[
         'License :: Public Domain',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
+        "Operating System :: OS Independent"
     ],
-    packages=find_packages( include=['brython', 'brython.*'] ),
 
-    python_requires='>=3.7, <3.10',
-    install_requires=[],
-    extras_require={}
+    package_dir={"": "src"},
+
+    python_requires='>=3.7, <3.10'
 )
